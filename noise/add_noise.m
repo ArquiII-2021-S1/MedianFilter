c0c8fb251e5d7f88244191c25d1f5818778e6b49
Arturo Cordoba <arturocv16@gmail.com>
2021-06-14 20:26:55 -0600

    Using noise function of matlab

--- noise/add_noise.m
+++ noise/add_noise.m
@@ -2,18 +2,14 @@ clc; clear; close all;
 pkg load image;
 
 % A = imread('edificio_china.png');
-A = imread('beach.png')(:,:,1);
+A = imread('video/frame50.png')(:,:,1);
 subplot(1,2,1);
-A = im2double(A);
 imshow(A);
 title('Imagen original');
 
-[m,n] = size(A);
-Z = salt_and_pepper(m, n, 0.1);
-M = A + Z;
+M = imnoise(A,'salt & pepper', 0.1);
 subplot(1,2,2);
 imshow(M);
 title('Ruido sal y pimienta');
 
-G = im2uint8(M);
-imwrite(G, 'ruido.png');
+imwrite(M, 'ruido.png');
